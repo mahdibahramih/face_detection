2254876a1242e58843ba2712693212db42fad568
mahdibahramih <mahdibahramih@gmail.com>
2019-11-02 10:21:53 +0330

    add some circle

--- face.py
+++ face.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 import numpy as np
 
 face = cv2.CascadeClassifier('face.xml')
@@ -10,11 +11,17 @@ while(cap.isOpened()):
 	faces = face.detectMultiScale(gray)
 	for (x,y,w,h) in faces:
 		cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-		roi_gray = gray[y:y+h,x:x+h]
+		cv2.circle(frame,(x+w,y-20),6,(255,255,255),-1,20)
+		cv2.circle(frame,(x+w+14,y-20-12),8,(255,255,255),-1,20)
+		cv2.circle(frame,(x+w+32,y-20-29),10,(255,255,255),-1,20)
+		cv2.circle(frame,(x+w+42,y-8),20,(255,0,0),1,20)
+		cv2.imshow('Frame',frame)
+		time.sleep(2)                
+		"""roi_gray = gray[y:y+h,x:x+h]
 		roi_color = frame[y:y+h,x:x+h]
 		eyes = eye.detectMultiScale(roi_gray)
 		for (ex,ey,ew,eh) in eyes:
-		    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
+                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)"""
 	cv2.imshow('Frame',frame)
 	if cv2.waitKey(25) & 0xFF == ord('q'):
 		break
